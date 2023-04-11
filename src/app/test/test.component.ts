cf6b4dec172777f836a616d0e2f7ff6bae7dfdff
clgleroux <clement@marketparts.com>
2023-04-11 10:55:55 +0200

    :wrench: openai test

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  styleUrls: ['./test.component.scss'],
 })
 export class TestComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  async ngOnInit(): Promise<void> {
+    const { Configuration, OpenAIApi } = require('openai');
+    const configuration = new Configuration({
+      apiKey: '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',
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: 'Say this is a test',
+      temperature: 0,
+      max_tokens: 7,
+    });
 
-  ngOnInit(): void {
+    console.log(response);
   }
-
 }
